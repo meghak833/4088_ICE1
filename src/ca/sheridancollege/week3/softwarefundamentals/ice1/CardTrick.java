fb4f9e24f4531a94c6324a75da93a3692a5a4f58
Alex <lowa@sheridancollege.ca>
2020-01-23 16:16:22 -0500

    Completed Assignment

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,28 +5,71 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
+ *
  * @author Megha Patel
+ * @author Alexander Low
+ * @studentNumber 991266865
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int) (Math.random() * 12) + 1);
+            c.setSuit(Card.SUITS[(int) (Math.random() * 3)]);
+            magicHand[i] = c;
         }
-        
+
         //insert code to ask the user for Card value and  or Hard code it, create their card
-        
-        //Then report the result here
+        try {
+            boolean found = false;
+            Scanner ln = new Scanner(System.in);
+            System.out.println("Please enter a card suit: ");
+            String suit = ln.nextLine().toLowerCase().substring(0, 0);
+            System.out.println("Please enter a card value: ");
+            int value = ln.nextInt();
+            for (int i = 0; i < magicHand.length; i++) {
+                if ((magicHand[i].getValue() == value)
+                        && (magicHand[i].getSuit().toLowerCase().substring(0, 0).equals(suit))) {
+                    found = true;
+                    System.out.println("Your card was found.");
+                    String cardValue;
+                    switch (i) {
+                        case 1:
+                            cardValue = "Ace";
+                            break;
+                        case 11:
+                            cardValue = "Jack";
+                            break;
+                        case 12:
+                            cardValue = "Queen";
+                            break;
+                        case 13:
+                            cardValue = "King";
+                            break;
+                        default:
+                            cardValue = Integer.toString(i);
+                    }
+                    System.out.println("It is the: " + cardValue + " of "
+                            + magicHand[i].getSuit());
+                } else {
+                    if (found == false && i == magicHand.length - 1) {
+                        System.out.println("Your card was not found");
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-    
+
 }
