4aefc6a2e87ff375766637985f624d4f2beaf3f3
Kiran Hart <kiran.hart@live.com>
2020-01-23 15:46:57 -0500

    step 7

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,7 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -21,6 +22,8 @@ import java.util.concurrent.ThreadLocalRandom;
 public class CardTrick {
 
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
 		
 		Card[] magicHand = new Card[7];
 		
@@ -37,8 +40,33 @@ public class CardTrick {
 		
 		// insert code to ask the user for Card value and or Hard code it, create their
 		// card
-
+		System.out.println("Enter card number:");
+		int cardNumber = 0;
+		while (!scanner.hasNextInt()) {
+			scanner.nextLine();
+			System.out.println("Enter card number: ");
+		}
+		cardNumber = scanner.nextInt();
+		if (cardNumber < 1) cardNumber = 1;
+		if (cardNumber > 13) cardNumber = 13;
+		
+		System.out.println("Enter Suit\n1 - Hearts\n2- Diamonds\n3- Spades\n4 - Clubs");
+		int cardType = 0;
+		while (!scanner.hasNextInt()) {
+			scanner.nextLine();
+			System.out.println("Enter Suit\n1 - Hearts\n2- Diamonds\n3- Spades\n4 - Clubs");	
+		}
+		cardType = scanner.nextInt();
+		if (cardType < 1) cardType = 0;
+		if (cardType > 4) cardType = 3;
+		
+		// setup the card
+		Card userCard = new Card();
+		userCard.setSuit(Card.SUITS[cardType - 1]);
+		userCard.setValue(cardNumber);
+		
 		// Then report the result here
+		System.out.println(userCard.getValue() + " of " + userCard.getSuit());
 	}
 
 }
