124e3a5c0e67effd79327091aa36d750a3536739
sing9710 <59976716+sing9710@users.noreply.github.com>
2020-01-23 16:19:34 -0500

    Created constructor with arguments.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -23,6 +23,12 @@ public class Card {
     /**
      * @return the suit
      */
+   
+   public Card(int value,String suit){
+      this.value=value;
+      this.suit=suit;
+   }
+   
     public String getSuit() {
         return suit;
     }
