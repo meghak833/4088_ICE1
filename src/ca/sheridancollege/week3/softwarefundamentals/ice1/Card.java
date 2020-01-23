6699094ae7237125002da28fa6e4a8857395bcca
kumritik <59710255+kumritik@users.noreply.github.com>
2020-01-23 16:19:38 -0500

    constructors was generated

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -12,7 +12,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author Megha Patel
+ * @author Ritik Kumar
  */
 public class Card {
 
@@ -23,6 +23,10 @@ public class Card {
     /**
      * @return the suit
      */
+   public Card(int value , String suit){
+      this.value=value;
+      this.suit=suit;
+      
     public String getSuit() {
         return suit;
     }
