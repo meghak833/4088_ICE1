425b5aad12b6a272418ae70082530da7f135308b
Ha Neul Moon <moonhan@sheirdancollege.ca>
2020-01-23 16:12:54 -0500

    Random added to generate 7 random cards

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,26 +5,32 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Random;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
+ * @modifier Ha Neul Moon
+ * student number: 991246554
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Random r = new Random();
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(r.nextInt(14));
+            c.setSuit(Card.SUITS[r.nextInt(4)]);
         }
         
         //insert code to ask the user for Card value and  or Hard code it, create their card
+        
         
         //Then report the result here
     }
