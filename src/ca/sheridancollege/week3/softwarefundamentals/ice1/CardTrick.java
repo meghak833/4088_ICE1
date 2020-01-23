e5ed4188356294ecb6f1e476e8e91d0ca8eb8e42
akavaramV <akavaram@sheridancollege.ca>
2020-01-23 16:04:07 -0500

    In Class Activity Pushing branch to repository

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,6 +10,8 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
+ * @modifier Vaishnav Akavaram
+ * @StudentID: 991578795
  */
 public class CardTrick {
     
@@ -17,11 +19,17 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
+        double randCard = (Math.random() * 13);
+        double randSuit = (Math.random() * 4);
+        
+        
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int) randCard);
+            c.setSuit(Card.SUITS[(int)randSuit]);   
         }
         
         //insert code to ask the user for Card value and  or Hard code it, create their card
