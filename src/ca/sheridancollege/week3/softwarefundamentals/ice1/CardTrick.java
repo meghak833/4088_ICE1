a4cbd792a68b02b00896d09bdf7801cbdfc1ba3d
Aditya Gadgil <gadgil@sheridancollege.com>
2020-01-23 16:02:28 -0500

    Part 3

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,6 +10,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
+ * @author Aditya Gadgil Student number 991384833
  */
 public class CardTrick {
     
@@ -20,9 +21,11 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random() * (13)));
+            c.setSuit(Card.SUITS[(int)(Math.random() * (3))]);
+           
         }
+        
         
         //insert code to ask the user for Card value and  or Hard code it, create their card
         
