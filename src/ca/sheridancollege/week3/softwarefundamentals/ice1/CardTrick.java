9af27b7610c3aa1a917e3467b9ffdd43b9009bd8
shengdao jin <jinshen@shernet.sheridancollege.ca>
2020-01-23 16:08:54 -0500

    User input for Lucky Card and comparing to MagicHand array created.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -27,9 +27,33 @@ public class CardTrick {
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         
+        System.out.println("The magic hand of 7 cards:");
         for (int i=0; i<magicHand.length; i++) {
-            System.out.println("The magic hand of 7 cards:");
             System.out.println(magicHand[i].getSuit() + " " + magicHand[i].getValue());
+        }
+        
+        Scanner sc = new Scanner(System.in);
+        Card luckyCard = new Card();
+        System.out.println("Please enter your Lucky Card's suit: ");
+        luckyCard.setSuit(sc.nextLine());
+        System.out.println("Please enter your Lucky Card's value: ");
+        luckyCard.setValue(sc.nextInt());
+        System.out.println("Your Lucky Card is: "+luckyCard.getSuit()+" "+
+                            luckyCard.getValue());
+        
+        boolean isLuckyCardInArray = false;
+        for (int i=0; i<magicHand.length; i++) {
+            if (magicHand[i].getSuit().equals(luckyCard.getSuit()) && 
+                    magicHand[i].getValue()==luckyCard.getValue()){
+                isLuckyCardInArray = true;
+            }
+        }
+        
+        if(isLuckyCardInArray) {
+            System.out.println("Your Lucky Card is in the Magic Hand!");
+        }
+        else {
+            System.out.println("Oops! Magic Hand does not have your Lucky Card.");
         }
         //insert code to ask the user for Card value and  or Hard code it, create their card
         
