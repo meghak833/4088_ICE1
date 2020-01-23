e8dc8dcd9b468260462f27cb56b8c19388f94248
Kiran Hart <kiran.hart@live.com>
2020-01-23 15:32:49 -0500

    Step 3

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,28 +5,36 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
+ * 
  * @author Megha Patel
+ * 
+ * 
+ * @author Kiran Hart
+ * @studentNumber 991564297
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
-        
-        //insert code to ask the user for Card value and  or Hard code it, create their card
-        
-        //Then report the result here
-    }
-    
+
+	public static void main(String[] args) {
+		
+		Card[] magicHand = new Card[7];
+		
+		for (int i = 0; i < magicHand.length; i++) {
+			Card c = new Card();
+			c.setValue(ThreadLocalRandom.current().nextInt(1, 14));
+			c.setSuit(Card.SUITS[ThreadLocalRandom.current().nextInt(0, 4)]);
+			magicHand[i] = c;
+		}
+		
+		// insert code to ask the user for Card value and or Hard code it, create their
+		// card
+
+		// Then report the result here
+	}
+
 }
