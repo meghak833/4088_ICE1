eca913e1225bb2370ef8ac98d5265391baa0a1a0
sing9710 <sing9710@sheridancollege.ca>
2020-01-23 22:48:34 -0500

    Imported Scanner to ask inputs from the user.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,37 +5,40 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
+ * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
+ *
  * @author Karandeep Singh
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-             c.setValue((int) (Math.random() * 13));
             //c.setValue(insert call to random number generator here)
-            c.setSuit(Card.SUITS[(int) (Math.random() * 3)]);
+            c.setValue(1 + (int) (Math.random() * 13));
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[1 + (int) (Math.random() * 3)]);
             System.out.println(c.getValue());
             System.out.println(c.getSuit());
         }
-        
-        Card c1=new Card();
-        c1.setValue(4);
-        c1.setSuit("diamond");
+
         //insert code to ask the user for Card value and  or Hard code it, create their card
-        
-        System.out.println("lucky number="+c1.getValue());
-        System.out.println("lucky suit="+c1.getSuit());
+        System.out.println("Enter lucky card number");
+        int value = sc.nextInt();
+        System.out.println("Enter lucky card suit");
+        String suit = sc.next();
+        Card c1 = new Card();
+        c1.setValue(value);
+        c1.setSuit(suit);
+
         //Then report the result here
+        System.out.println("Lucky number: " + c1.getValue());
+        System.out.println("Lucky suit: " + c1.getSuit());
     }
-    
+
 }
